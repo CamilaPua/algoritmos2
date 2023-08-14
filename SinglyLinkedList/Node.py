d61d2921d39d6e8cae4b1e054a375325fa8970dd
ARCINIEGAS PUA MARIA <marcinie3@cuc.edu.co>
2023-08-14 15:48:07 -0500

    first class 14/08/2023

--- SinglyLinkedList/Node.py
+++ SinglyLinkedList/Node.py
@@ -1,10 +1,21 @@
-# Simple node class
+# Single node class
 class Node:
     def __init__(self, data) -> None:
         self.data = data
         self.next = None
 
 
-if __name__ == '__main__':
-    pass
+    def __str__(self) -> str:
+        return str(self.data)
 
+
+# Double node class
+class DoubleNode(Node):
+    def __init__(self, data) -> None:
+        super.__init__(self, data)
+        self.previous = None
+
+
+if __name__ == '__main__':
+    nodo01 = Node('Casa')
+    print(f'Nodo: {nodo01}')
--- SinglyLinkedList/SinglyLinkedList.py
+++ SinglyLinkedList/SinglyLinkedList.py
@@ -1,3 +1,29 @@
+from Node import Node
+
+# Singly linked list class
+class SinglyLinkedList:
+    def __init__(self) -> None:
+        self.head = None
+
+    def list_is_empty(self):
+        return self.head == None
+
+    def append_at_beginning(self, data):
+        new_node = Node(data)
+        if self.list_is_empty():
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head     = new_node    # Update list head
+
+    def __str__(self) -> str:
+        route = '[ '
+        current_node = self.head
+        while current_node != None:
+            route += str(current_node) + ' '
+            current_node = current_node.next
+        return route + ']'
+
+
 if __name__ == '__main__':
     pass
-
